a2e4cb9173fe0921a9f3ff5a7a6c36c38af54db9
Govind singh <144331422+go-win0001@users.noreply.github.com>
2025-07-23 20:19:13 +0530

    Update axios.js
    Added the Live Backend Url , via render

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://learning-site-fj22.onrender.com/api";
+// https://learning-site-fj22.onrender.com
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
